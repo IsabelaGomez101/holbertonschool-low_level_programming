9cd54245a55e8bb21e2a0d878568cef24f8f7f6d
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-18 19:10:42 +0000

    number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ int main(void)
   
 int n;
 
-srand time (0);
-n = rand () - RAND_MAX / 2;
+srand(time(0);
+n = rand() - RAND_MAX / 2;
 
 if (n == 0)
-printf("is zero\n", n);
+printf("%d is zero\n", n);
 else if (n > 0)
 printf("%d is positive\n", n);
 else
