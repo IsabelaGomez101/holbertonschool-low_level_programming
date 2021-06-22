4c79b853d40c59b1bd39a719678b130cb9e67534
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-22 19:34:55 +0000

    prints every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day.
+ *
+ * Return: hour.
+ */
+
+void jack_bauer(void)
+{
+int H, h, M, m;
+for (H = 0; H <= 2; H++)
+{
+for (h = 0; h <= 3; h++)
+{
+for (M = 0; M <= 5; M++)
+{
+for (m = 0 ; m <= 9; m++)
+{
+_putchar(H + '0');
+_putchar(h + '0');
+_putchar(':');
+_putchar(M + '0');
+_putchar(m + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
