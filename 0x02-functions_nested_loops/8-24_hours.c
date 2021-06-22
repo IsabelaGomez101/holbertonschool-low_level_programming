c3a98a48c4e3814751afa562d471b641e8d138df
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-22 20:00:18 +0000

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,14 @@ void jack_bauer(void)
 int H, h, M, m;
 for (H = 0; H <= 2; H++)
 {
-for (h = 0; h <= 3; h++)
+for (h = 0; h <= 9; h++)
 {
 for (M = 0; M <= 5; M++)
 {
 for (m = 0 ; m <= 9; m++)
 {
+if (H >= 2 && h >= 4)
+break;
 _putchar(H + '0');
 _putchar(h + '0');
 _putchar(':');
