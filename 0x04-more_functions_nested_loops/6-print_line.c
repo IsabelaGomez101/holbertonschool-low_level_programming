b1a662f7650da80872b819a28f864408bcd195dc
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-25 03:29:51 +0000

    function thath draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - write a function that draws a straight line
+ * @n: integer.
+ * Return: void.
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
