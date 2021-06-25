7c6677bc8c6d918f0a9474118389b4f593879ea3
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-25 15:54:45 +0000

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,15 @@ void print_square(int size)
 {
 int i;
 int c;
+if (size <= 0)
+{
+_putchar('\n');
+}
 for (i = 0; i < size; i++)
 {
 for (c = 0; c < size; c++)
 {
-if (size > 0)
-{
 _putchar('#');
-}
-else
-{
-_putchar('\n');
-}
 }
 _putchar('\n');
 }
