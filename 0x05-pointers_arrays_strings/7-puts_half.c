0209224a603bd6b98b5928fac355d2ac6650f867
IsabelaGomez101 <3379@holbertonschool.com>
2021-06-29 19:46:09 +0000

    print the second half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print the second half of the string.
+ * @str: string
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int n;
+while (*(str + i) != '\0')
+{
+i++;
+}
+if ((i % 2) != 0)
+{
+n = (i - 1) / 2;
+}
+else
+{
+n = i / 2;
+}
+while (n <= i)
+{
+_putchar(str[n]);
+n++;
+}
+_putchar('\n');
+}
