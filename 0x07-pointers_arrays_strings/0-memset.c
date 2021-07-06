f1882231bdc852a6222aeee1fb305e028a5b285c
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-05 20:16:25 -0500

    fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* *_memset - function that fills memory with a constant byte.
+* @s: A pointer to the memory area to be filled.
+* @b: The character to fill the memory area with.
+* @n: The number of bytes to be filled.
+*
+* Return: A pointer to the filled memory area @s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (0);
+}
