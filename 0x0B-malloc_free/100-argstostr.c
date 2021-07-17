c389956b6c8b214afdca09d9b05c5dd7d602867b
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-17 16:27:14 -0500

    concatenates all the arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,7 +14,7 @@ char *argstostr(int ac, char **av)
 char *str;
 int i;
 int j;
-int s;
+int s = 0;
 int c = 0;
 
 if (ac == 0 || av == NULL)
@@ -26,9 +26,10 @@ for (i = 0; i < ac; i++)
 for (j = 0; av[i][j] != '\0'; j++)
 {
 }
-s += j;
+s = s + j;
 }
-str = malloc(sizeof(char) * s + 1 + ac);
+s++;
+str = malloc(sizeof(char) * s);
 if (str == NULL)
 {
 return (NULL);
@@ -37,10 +38,12 @@ for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
 {
-str[c++] = av[i][j];
+str[c] = av[i][j];
+c++;
 }
-str[c++] = '\n';
+str[c] = '\n';
+c++;
 }
-str[c++] = '\0';
+str[c] = '\0';
 return (str);
 }
