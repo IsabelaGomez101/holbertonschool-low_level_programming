488a4b495ea9888f19a1980c816d63c763f566ad
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-19 15:27:21 -0500

    prints the name of the file it was compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - program that prints the name of the file it was compiled
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("%s\n", FILENAME);
+return (0);
+}
