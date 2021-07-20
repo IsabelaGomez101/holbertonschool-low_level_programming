8a7f90ebf01097702810a1700ec6b8fc6a6dfe35
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-19 19:20:20 -0500

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+struct dog my_dog(char *name, float age, char *owner);
+/**
+ * my_dog - check the code for Holberton School students.
+ * @name:
+ * @age:
+ * @owner:
+ * Description:
+ */
+
+#endif /*#ifndef _DOG_H*/
