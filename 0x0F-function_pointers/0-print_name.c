bf5fec5b5af01f1f392829c7249e3cf80d2253a3
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-21 20:53:07 -0500

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_name - function that prints a name.
@@ -11,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL && name != NULL && name != '\0')
+if (f != NULL && name != NULL && *name != '\0')
 f(name);
 }
