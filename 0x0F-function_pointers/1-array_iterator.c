444bfd63f1ecc805b31bc81eb42c765b2b7c5f9b
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-21 21:27:22 -0500

    function that executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator- function that executes a function.
+ * @array: pointer toarray
+ * @size: size of the array
+ * @action: pointer to function
+ * Return: void.
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+if (array != NULL && action != NULL && size > 0)
+{
+for (i = 0; i < size; i++)
+{
+(*action)(array[i]);
+}
+}
+}
