2b5bbc323609d9b3d45c35bd7f7cda38d05964b2
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-23 00:18:54 -0500

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,26 +11,25 @@
 
 int main(int argc, char *argv[])
 {
+int (*f)(int a, int b);
 int num1, num2;
 int calc;
 char *operator = argv[2];
-if (argc != 4)
+
+if (argc > 4 || argc < 4)
 {
-printf("Error98\n");
-printf("%d\n", argc);
+printf("Error\n");
 exit(98);
-}
-if ((*operator != '+' && *operator != '-' && *operator != '*'
-&& *operator != '/' && *operator != '%') || operator[1] != '\0')
-{
-printf("Error99\n");
-exit(99);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-calc = get_op_func(operator)(num1, num2);
+f = get_op_func(operator);
+if (!f)
 {
-printf("%d\n", calc);
+printf("Error\n");
+exit(99);
 }
+calc = f(num1, num2);
+printf("%d\n", calc);
 return (0);
 }
