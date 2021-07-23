f6b357c245aefca0018b592e907ff91789fd44eb
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-22 23:24:01 -0500

    calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - function
+ * @argc: argument
+ * @argv: argument
+ * Return: result.
+ */
+
+int main(int argc, char *argv[])
+{
+int num1, num2;
+int calc;
+char *operator = argv[2];
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (*operator != '+' && *operator != '-' && *operator != '*'
+&& *operator != '/' && *operator != '%')
+{
+printf("Error\n");
+exit(99);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+calc = get_op_func(operator)(num1, num2);
+{
+printf("%d\n", calc);
+}
+return (0);
+}
