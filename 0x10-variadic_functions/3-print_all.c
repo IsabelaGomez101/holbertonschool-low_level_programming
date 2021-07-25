27700cdd24724c0ee06bb5b8ccbb5e5733bc9a87
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-24 22:18:17 -0500

    modified file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,60 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
 #include "variadic_functions.h"
-
-/*FUNCTIONS TO PRINT*/
-
-/**
- * p_char - function that prints type char.
- * @arg: list of arguments passed to the function.
- * Return: void.
- */
-void p_char(va_list arg)
-{
-	printf("%c", va_arg(arg, int));
-}
-
-/**
- * p_int - function that prints type int.
- * @arg: list of arguments passed to the function.
- * Return: void.
- */
-void p_int(va_list arg)
-{
-	printf("%d", va_arg(arg, int));
-}
-
-/**
- * p_float - function that prints type float.
- * @arg: list of arguments passed to the function.
- * Return: void.
- */
-void p_float(va_list arg)
-{
-	printf("%f", va_arg(arg, double));
-}
-
-/**
- * p_str - function that prints type string.
- * @arg: list of arguments passed to the function.
- * Return: void.
- */
-void p_str(va_list arg)
-{
-	char *s;
-
-	s = va_arg(arg, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	else
-	{
-		printf("%s", s);
-	}
-}
+#include "3-functions_print.h"
 
 /*MY FUNCTION*/
 /**
