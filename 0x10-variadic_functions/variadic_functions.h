cfd1cf34b79ff726f5977504521535e44f1ffa4d
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-24 21:39:40 -0500

    modified lines, header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,30 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/*PROTOTYPES TASK 3*/
+
+/**
+ * struct printypes - structure containing two variables
+ * @tp: Type of argument.
+ * @f: The function associated.
+ */
+typedef struct printypes
+{
+	char *tp;
+	void (*f)(va_list arguments);
+} pr_t;
+
+void printchar(va_list arg);
+void printint(va_list arg);
+void printfloat(va_list arg);
+void printstring(va_list arg);
+void print_all(const char *const format, ...);
 
 #endif /*variadic functions*/
