93fb64695d9ce6f8404a90ad07b07136e722e613
IsabelaGomez101 <3379@holbertonschool.com>
2021-08-03 15:13:26 -0500

    add file listint len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,12 +11,10 @@ size_t listint_len(const listint_t *h)
 {
 const listint_t *temp = NULL;
 int i = 0; /**counter number of nodes*/
-int num;
 
 temp = h;
 while (temp)
 {
-num = temp->n;
 i++;
 temp = temp->next;
 }
