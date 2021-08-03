79f78a9b029351c9539b33a18295de6b9d57273e
IsabelaGomez101 <3379@holbertonschool.com>
2021-08-03 16:31:52 -0500

    add file free listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 
 /**
  * free_listint2 - function that frees a listint_t list.
- * @head: listint_t list.
+ * @head: double pointer to head of list.
  * Return: void.
 */
 
@@ -12,14 +12,14 @@ void free_listint2(listint_t **head)
 listint_t *temp;
 listint_t *temp2;
 
-temp2 = *head;
 if (head == NULL)
 return;
+temp2 = *head;
 	while (temp2 != NULL)
 	{
 		temp = temp2;
-		temp2 = temp2->next;
 		free(temp);
+		temp2 = temp2->next;
 	}
 	*head = NULL;
 }
