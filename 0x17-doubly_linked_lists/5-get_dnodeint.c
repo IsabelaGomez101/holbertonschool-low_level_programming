97057ea2661abffb6cc534944c24fd9e7d545796
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-17 00:15:02 -0500

    adding file get dnodeint , correction betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,25 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - function that returns the nth node
+ * of a dlistint_t linked list.
+ * @head: node head.
+ * @index: index of the node, starting from 0.
+ * Return: the node or NULL
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current;
 	unsigned int len = 0;
 
-    current = head;
+current = head;
 	while (current != NULL)
 	{
-	  if (len == index)
-	  return (current);
-	  len++;
-	  current = current->next;
+	if (len == index)
+	return (current);
+	len++;
+	current = current->next;
 	}
-    return (NULL);
+	return (NULL);
 }
